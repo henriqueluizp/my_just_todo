206e143f4f0621248544a6ba23e07ea1298b0e58
Henrique <henrique.pintoluiz@gmail.com>
2026-06-18 22:11:03 -0300

    Initial Commit

---
+++ minimalist_todo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  minimalist_todo
+//
+//  Created by Henrique on 18/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
